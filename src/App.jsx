4fdc895b3c3dcbc911907c0882187047ceb59c17
Lawrence Caulker <locaulker@gmail.com>
2023-09-27 12:10:28 -0400

    setup form structure with controlled inputs

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
-const App = () => {
-  return <h2>Grocery Bud - Starter</h2>;
-};
+import { useState } from "react"
+import Form from "./Form"
 
-export default App;
+const App = () => {
+  const [items, setItems] = useState([])
+
+  return (
+    <section className="section-center">
+      <Form />
+    </section>
+  )
+}
+
+export default App
